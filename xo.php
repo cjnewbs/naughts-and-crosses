e96638cfa01007e6aaa6cc4b30b6d021d69f5d5c
Craig Newbury <craig@newbury.me>
2024-03-16 16:13:54 +0000

    Basic placing of moves on board

--- xo.php
+++ xo.php
@@ -3,6 +3,7 @@
 class XO
 {
     private $stdin;
+    private string $places;
 
     public function __construct()
     {
@@ -14,15 +15,19 @@ class XO
     public function play()
     {
         $this->clearScreen();
+        $this->resetGameBoard();
+        $this->drawGameBoard();
         while (true) {
             $keypress = $this->readInput();
             if ($keypress === "\e") {
+                $this->clearScreen();
                 echo 'Goodbye...' . PHP_EOL;
                 return;
             }
-            if ($keypress) {
+            if ($keypress && in_array($keypress, ['1', '2', '3', '4', '5', '6', '7', '8', '9'])) {
+                $this->writeMove($keypress);
                 $this->clearScreen();
-                echo 'Key pressed: ' . $this->translateInput($keypress) . PHP_EOL;
+                $this->drawGameBoard();
             }
         }
     }
@@ -32,35 +37,76 @@ class XO
         return fgets($this->stdin);
     }
 
-    private function translateInput($string)
-    {
-        switch ($string) {
-            case "\033[A":
-                return "UP";
-            case "\033[B":
-                return "DOWN";
-            case "\033[C":
-                return "RIGHT";
-            case "\033[D":
-                return "LEFT";
-            case "\n":
-                return "ENTER";
-            case " ":
-                return "SPACE";
-            case "\010":
-            case "\177":
-                return "BACKSPACE";
-            case "\t":
-                return "TAB";
-            case "\e":
-                return "ESC";
-        }
-        return $string;
-    }
-
     private function clearScreen()
     {
         echo chr(27) . chr(91) . 'H' . chr(27) . chr(91) . 'J';
+    }
+
+    private function drawGameBoard()
+    {
+        $board = <<<BOARD
+|-----------|
+| %s | %s | %s |
+|---|---|---|
+| %s | %s | %s |
+|---|---|---|
+| %s | %s | %s |
+|-----------|
+
+BOARD;
+        echo sprintf(
+            $board,
+            $this->places[0],
+            $this->places[1],
+            $this->places[2],
+            $this->places[3],
+            $this->places[4],
+            $this->places[5],
+            $this->places[6],
+            $this->places[7],
+            $this->places[8],
+        );
+
+    }
+
+    private function resetGameBoard()
+    {
+        $this->places = '         ';
+    }
+
+    private function writeMove($keypress)
+    {
+        switch ($keypress) {
+            case '7':
+                $index = 0;
+                break;
+            case '8':
+                $index = 1;
+                break;
+            case '9':
+                $index = 2;
+                break;
+            case '4':
+                $index = 3;
+                break;
+            case '5':
+                $index = 4;
+                break;
+            case '6':
+                $index = 5;
+                break;
+            case '1':
+                $index = 6;
+                break;
+            case '2':
+                $index = 7;
+                break;
+            case '3':
+                $index = 8;
+                break;
+        }
+        $this->resetGameBoard();
+        $this->places[$index] = 'X';
     }
 }
 (new XO())->play();
