c0eb3f9d2f6c87734ea83db07473df8d733f7a5b
Craig Newbury <craig@newbury.me>
2024-03-16 16:52:00 +0000

    both players can now place moves in unused positions

--- xo.php
+++ xo.php
@@ -4,6 +4,7 @@ class XO
 {
     private $stdin;
     private string $places;
+    private string $currentPlayer = 'O';
 
     public function __construct()
     {
@@ -24,10 +25,8 @@ class XO
                 echo 'Goodbye...' . PHP_EOL;
                 return;
             }
-            if ($keypress && in_array($keypress, ['1', '2', '3', '4', '5', '6', '7', '8', '9'])) {
-                $this->writeMove($keypress);
-                $this->clearScreen();
-                $this->drawGameBoard();
+            if (in_array($keypress, ['1', '2', '3', '4', '5', '6', '7', '8', '9'])) {
+                $this->playMove($keypress);
             }
         }
     }
@@ -40,6 +39,37 @@ class XO
     private function clearScreen()
     {
         echo chr(27) . chr(91) . 'H' . chr(27) . chr(91) . 'J';
+    }
+
+    private function playMove($keypress)
+    {
+        $index = $this->mapIndex($keypress);
+        if ($this->places[$index] === ' ') {
+            $this->places[$index] = $this->currentPlayer;
+            $this->clearScreen();
+            $this->drawGameBoard();
+            $this->togglePlayer();
+        } else {
+            $this->clearScreen();
+            $this->drawGameBoard();
+            echo 'Move cannot be played';
+        }
+
+    }
+
+    private function mapIndex($keypress)
+    {
+        return match($keypress) {
+            '7' => 0,
+            '8' => 1,
+            '9' => 2,
+            '4' => 3,
+            '5' => 4,
+            '6' => 5,
+            '1' => 6,
+            '2' => 7,
+            '3' => 8,
+        };
     }
 
     private function drawGameBoard()
@@ -66,21 +96,9 @@ BOARD;
         $this->places = '         ';
     }
 
-    private function writeMove($keypress)
+    private function togglePlayer()
     {
-        $index = match($keypress) {
-            '7' => 0,
-            '8' => 1,
-            '9' => 2,
-            '4' => 3,
-            '5' => 4,
-            '6' => 5,
-            '1' => 6,
-            '2' => 7,
-            '3' => 8,
-        };
-        $this->resetGameBoard();
-        $this->places[$index] = 'X';
+        $this->currentPlayer === 'O' ? $this->currentPlayer = 'X' : $this->currentPlayer = 'O';
     }
 }
 (new XO())->play();
